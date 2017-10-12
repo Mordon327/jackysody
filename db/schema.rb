5c3dd2f08e0a9a3db62c93d819b21829c020a5cb
Admin <admin@Admins-MBP.ad.uvu.edu>
2017-10-12 16:39:30 -0600

    Created portfolios with resource

--- db/schema.rb
+++ db/schema.rb
@@ -22,16 +22,6 @@ ActiveRecord::Schema.define(version: 20171012222821) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "porftfolios", force: :cascade do |t|
-    t.string "title"
-    t.string "subtitle"
-    t.text "body"
-    t.text "main_image"
-    t.text "thumb_image"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "portfolios", force: :cascade do |t|
     t.string "title"
     t.string "subtitle"
