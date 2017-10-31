41a187d33df9285ae179f382c1ecb546eda2dc5d
Admin <soderstromshane@gmail.com>
2017-10-31 15:49:44 -0600

    refactored a ton of crap

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -2,8 +2,7 @@ class Portfolio < ApplicationRecord
 	has_many :technologies
 	accepts_nested_attributes_for :technologies, reject_if: lambda { |attrs| attrs['name'].blank? }
 
-	include Placeholder
-	validates_presence_of :title, :subtitle, :main_image, :thumb_image
+	validates_presence_of :title, :subtitle
 
 	mount_uploader :thumb_image, PortfolioUploader
 	mount_uploader :main_image, PortfolioUploader
@@ -18,11 +17,4 @@ class Portfolio < ApplicationRecord
 
 	#custom scope.  Place the scope name ruby_on_rails_portfolio_items in the portfolios_controller rb in the index function as Portfolio.ruby_on_rails_portfolio_items instead of Portfolio.all
 	scope :ruby_on_rails_portfolio_items, -> { where(subtitle: 'Ruby on rails')}
-
-	after_initialize :set_defaults
-
-	def set_defaults
-		self.main_image ||= Placeholder.image_generator(height: '600', width: '400')
-		self.thumb_image ||= Placeholder.image_generator(height: '350', width: '200')
-	end
 end
