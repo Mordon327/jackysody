aa2cf227ec87cc81770419d96445252f8bbc74c8
Admin <soderstromshane@gmail.com>
2017-11-06 11:29:37 -0700

    created topic drag down box

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -2,8 +2,8 @@ class Blog < ApplicationRecord
 	enum status: { draft: 0, published: 1}
 	extend FriendlyId
 	friendly_id :title, use: :slugged
-	validates_presence_of :title, :body
-	#belongs_to :topic
+	validates_presence_of :title, :body, :topic_id
+	belongs_to :topic
 	has_many :comments, dependent: :destroy
 
 	def self.special_blogs
